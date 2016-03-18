946f14f40a1530fe07606f8de2cb4c32a3954bc0
Benjamin HUBERT <benjamin@alpixel.fr>
2016-03-18 10:27:59 +0100

    comply to sensio labs insight

--- AlpixelCMSBundle.php
+++ AlpixelCMSBundle.php
@@ -2,8 +2,6 @@
 
 namespace Alpixel\Bundle\CMSBundle;
 
-use Symfony\Component\DependencyInjection\Container;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AlpixelCMSBundle extends Bundle
