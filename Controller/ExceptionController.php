422e48e86b2330101e06733922412ec22679dd7b
Benjamin HUBERT <benjamin@alpixel.fr>
2016-03-18 10:12:31 +0100

    added exception controller

---
+++ Controller/ExceptionController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Controller;
+
+use Symfony\Bundle\TwigBundle\Controller\ExceptionController as BaseController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\FlattenException;
+
+class ExceptionController extends BaseController
+{
+    /**
+     * @Route("/erreur", name="error")
+     */
+    public function showAction(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null)
+    {
+        $currentContent = $this->getAndCleanOutputBuffering($request->headers->get('X-Php-Ob-Level', -1));
+        $showException = $request->attributes->get('showException', $this->debug); // As opposed to an additional parameter, this maintains BC
+
+        $code = $exception->getStatusCode();
+
+        return new Response($this->twig->render(
+            (string) $this->findTemplate($request, $request->getRequestFormat(), $code, $showException),
+            array(
+                'status_code' => $code,
+                'status_text' => isset(Response::$statusTexts[$code]) ? Response::$statusTexts[$code] : '',
+                'exception' => $exception,
+                'logger' => $logger,
+                'currentContent' => $currentContent,
+            )
+        ));
+    }
+
+    /**
+     * @param Request $request
+     * @param string  $format
+     * @param int     $code          An HTTP response status code
+     * @param bool    $showException
+     *
+     * @return string
+     */
+    protected function findTemplate(Request $request, $format, $code, $showException)
+    {
+        $name = $showException ? 'exception' : 'error';
+        if ($showException && 'html' == $format) {
+            $name = 'exception_full';
+        }
+
+        // For error pages, try to find a template for the specific HTTP status code and format
+        if (!$showException) {
+            $template = sprintf('@Twig/Exception/%s%s.%s.twig', $name, $code, $format);
+            if ($this->templateExists($template)) {
+                return $template;
+            }
+        }
+
+        // try to find a template for the given format
+        $template = sprintf('@Twig/Exception/%s.%s.twig', $name, $format);
+        if ($this->templateExists($template)) {
+            return $template;
+        }
+
+        // default to a generic HTML exception
+        $request->setRequestFormat('html');
+
+        return sprintf('@Twig/Exception/%s.html.twig', $showException ? 'exception_full' : $name);
+    }
+}
