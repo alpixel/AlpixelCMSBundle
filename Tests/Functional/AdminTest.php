e86a57720a7cf88d9e191ebc8269070ae9d9d291
Benjamin HUBERT <benjamin@alpixel.fr>
2016-04-01 18:50:52 +0200

    first test ok

--- Tests/Functional/AdminTest.php
+++ Tests/Functional/AdminTest.php
@@ -12,7 +12,7 @@ class AdminTest extends BaseTestCase
     public function testTranschoiceWhenTranslationNotYetExtracted()
     {
         $client = $this->createClient();
-        $client->request('GET', '/admin');
+        $client->request('GET', '/admin/dashboard?_locale=en');
         $response = $client->getResponse();
         $this->assertEquals(200, $response->getStatusCode(), substr($response, 0, 2000));
     }
