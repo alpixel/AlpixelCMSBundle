9f84ce4256024427a2b4e10f3cdf02dd7383fa6d
Benjamin Hubert <benjamin@alpixel.fr>
2016-04-08 14:14:31 +0200

    Applied fixes from StyleCI (#28)

--- Tests/Functional/BaseTestCase.php
+++ Tests/Functional/BaseTestCase.php
@@ -1,16 +1,15 @@
 <?php
 
-
 namespace Alpixel\Bundle\CMSBundle\Tests\Functional;
-use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
 
+use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
 
 /**
  * @author Benjamin HUBERT <benjamin@alpixel.fr>
  */
 class BaseTestCase extends WebTestCase
 {
-    static protected function createKernel(array $options = array())
+    protected static function createKernel(array $options = [])
     {
         return new AppKernel(
             isset($options['config']) ? $options['config'] : 'config.yml'
