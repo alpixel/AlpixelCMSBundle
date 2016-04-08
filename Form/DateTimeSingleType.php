28de655d81f5aee0df62b30d2696d745ed11c57f
Alexis Bussières <bussieres.alexis.dev@gmail.com>
2016-04-08 13:57:57 +0200

    Applied fixes from StyleCI (#26)

--- Form/DateTimeSingleType.php
+++ Form/DateTimeSingleType.php
@@ -1,22 +1,20 @@
 <?php
 
-
 namespace Alpixel\Bundle\CMSBundle\Form;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
 /**
- * Class DateTimeSingleType
- * @package Alpixel\Bundle\CMSBundle\Form
+ * Class DateTimeSingleType.
  */
 class DateTimeSingleType extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'format' => 'd-m-Y H:i',
+            'format'      => 'd-m-Y H:i',
             'date_widget' => 'single_text',
             'time_widget' => 'single_text',
         ]);
