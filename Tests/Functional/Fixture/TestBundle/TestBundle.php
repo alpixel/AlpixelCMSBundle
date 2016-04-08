91f55723639e846efe69b721cf777ecbb4cbc2b5
Benjamin Hubert <benjamin@alpixel.fr>
2016-04-08 14:42:10 +0200

    Adding functionnal tests (#27)

---
+++ Tests/Functional/Fixture/TestBundle/TestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Alpixel\Bundle\CMSBundle\Tests\Functional\Fixture\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TestBundle extends Bundle
+{
+}
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace {
-    if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    if (!$loader = @include __DIR__.'/Functional/autoload.php') {
         echo <<<'EOM'
 You must set up the project dependencies by running the following commands:
 
