2f6a848a6565cfa7a17c0e94dce0c0a57a5703ca
Benjamin HUBERT <benjamin@alpixel.fr>
2016-04-20 17:03:23 +0200

    New login feature

--- Tests/Functional/Admin/AdminBlockTest.php
+++ Tests/Functional/Admin/AdminBlockTest.php
@@ -3,28 +3,74 @@
 namespace alpixel\cmsbundle\Tests\Functional\Admin;
 
 use Alpixel\Bundle\CMSBundle\Tests\Functional\BaseTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @author Alexis BUSSIERES <alexis@alpixel.fr>
  */
 class AdminBlockTest extends BaseTestCase
 {
-    public function testConfiguresRoute()
+    public function __construct()
     {
-        $client = $this->createAuthentifiedClient();
-
-        $client->request('GET', 'admin/block/list');
-        $this->assertTrue($client->getResponse()->isSuccessful());
-
-        $client->request('PUT', 'admin/block/1/edit');
-//        $this->assertTrue($client->getResponse()->isSuccessful());
-
-        var_dump($client->getResponse()->getStatusCode());
-        $client->request('GET', 'admin/block/1/delete');
-        var_dump($client->getResponse()->getStatusCode());
-//        $this->assertTrue($client->getResponse()->isSuccessful());
+        parent::__construct();
+        $this->logIn($this->getUser('ROLE_SUPER_ADMIN'), new Response());
+    }
 //
-//        $client->request('GET', 'admin/block/list');
-//        $this->assertTrue($client->getResponse()->isSuccessful());
+//    public function testConfiguresRoute()
+//    {
+        // SUCCESS
+//        $this->client->request('GET', 'admin/block/list');
+//        $this->assertTrue($this->client->getResponse()->isSuccessful());
+//
+//        $crawler = $this->client->request('GET', 'admin/block/1/edit');
+//        $this->assertTrue($this->client->getResponse()->isSuccessful());
+//
+//        $this->client->request('GET', 'admin/block/create');
+//        $this->assertTrue($this->client->getResponse()->isNotFound());
+//
+//        $this->client->request('GET', 'admin/block/batch');
+//        $this->assertTrue($this->client->getResponse()->isNotFound());
+//
+//        $this->client->request('GET', 'admin/block/export');
+//        $this->assertTrue($this->client->getResponse()->isNotFound());
+//
+//        $this->client->request('GET', 'admin/block/1/delete');
+//        $this->assertTrue($this->client->getResponse()->isNotFound());
+//
+//        $this->client->request('GET', 'admin/block/1/show');
+//        $this->assertTrue($this->client->getResponse()->isNotFound());
+//    }
+
+    public function testConfigureListFields()
+    {
+
+        $crawler = $this->client->request('GET', 'admin/block/1/edit');
+//        var_dump($this->client->getResponse()->getContent());
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+
+//        var_dump($client->getResponse()->getContent());die;
+//        $this->assertContains('Gestion des blocs', $client->getResponse()->getContent());
+//        $this->assertCount(1, $crawler->filter('div.form-group'));
+//        $editButton = $crawler->selectButton('btn_update_and_edit');
+//        $contentField = $crawler->filter('textarea')->extract(['name']);
+//        $form = $editButton->form([
+//            $contentField[0] => 'LOLOLO',
+//        ]);
+//        $form = $editButton->form();
+//        $this->client->submit($form);
+//        var_dump($this->client->getResponse()->getContent());
+//        var_dump(self::$client->getContainer()->get('security.token_storage')->getToken());
+//        var_dump($this->container->get('security.token_storage')->getToken());
+//        var_dump($client->getResponse()->getContent());
+//        $client = static::createClient();
+//        $crawler = $client->request('GET', '/admin/login');
+//        $buttonCrawlerNode = $crawler->selectButton('_submit');
+//        $form = $buttonCrawlerNode->form(array('_username' => 'alpixel','_password' => 'alpixel'));
+//        $client->submit($form);
+//        var_dump($client->getResponse()->getContent());
+//        $crawler = $client->followRedirect();
+//        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+//        $crawler = $client->request('GET', 'admin/block/1/edit');
+//
     }
 }
--- Tests/Functional/BaseTestCase.php
+++ Tests/Functional/BaseTestCase.php
@@ -3,10 +3,15 @@
 namespace Alpixel\Bundle\CMSBundle\Tests\Functional;
 
 use FOS\UserBundle\Doctrine\UserManager;
+use FOS\UserBundle\Model\User;
 use Symfony\Bridge\Doctrine\Security\User\EntityUserProvider;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\BrowserKit\CookieJar;
 use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 /**
@@ -14,15 +19,78 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
  */
 class BaseTestCase extends WebTestCase
 {
-    protected function logIn($client)
-    {
-        /** @var Container $container */
-        $container = $client->getContainer();
-        $session = $container->get('session');
+    /**
+     * @var \Symfony\Bundle\FrameworkBundle\Client
+     */
+    protected $client;
+    /**
+     * @var null|\Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    protected $container;
+    /**
+     * @var MockFileSessionStorage
+     */
+    protected $storage;
+    /**
+     * @var Session
+     */
+    protected $session;
 
-        $token = new UsernamePasswordToken('jean josé', null, 'admin', ['ROLE_SUPER_ADMIN']);
-        $container->get('security.token_storage')->setToken($token);
-        $container->get('session')->set('_security_main', serialize($token));
+
+    public function __construct()
+    {
+        $this->client = static::createClient();
+        $this->container = $this->client->getContainer();
+        $this->storage = new MockFileSessionStorage(__DIR__ . '/cache/sessions');
+        $this->session = new Session($this->storage);
+    }
+
+    public function getUser($role = null)
+    {
+        if (!isset($this->user)) {
+            $user = $this->getUserManager()->findUserByUsername('alpixel');
+
+            if (isset($user)) {
+                $this->user = $user;
+            } else {
+                $this->user = $this->getUserManager()->createUser();
+
+                $this->user->setEnabled(true);
+                $this->user->setFirstname('alpixel');
+                $this->user->setLastname('alpixel');
+                $this->user->setUsername('alpixel');
+                $this->user->setEmail('benjamin@alpixel.fr');
+                $this->user->setPlainPassword('alpixel');
+                $this->getUserManager()->updatePassword($this->user);
+                if (isset($role)) {
+                    $this->user->addRole($role);
+                }
+                $this->getUserManager()->updateUser($this->user);
+            }
+        }
+
+        return $this->user;
+    }
+
+    public function logIn(User $user, Response $response)
+    {
+        $this->session->start();
+
+        $firewallName = 'admin';
+
+        $this->cookie = new Cookie($this->storage->getName(), $this->storage->getId());
+        $this->cookieJar = new CookieJar();
+        $this->cookieJar->set($this->cookie);
+
+        $this->token = new UsernamePasswordToken($user, 'alpixel', $firewallName, $user->getRoles());
+        $this->session->set('_security_' . $firewallName, serialize($this->token));
+
+        $this->getSecurityManager()->loginUser($firewallName,
+            $user,
+            $response
+        );
+
+        $this->session->save();
     }
 
     protected static function createKernel(array $options = [])
@@ -32,11 +100,17 @@ class BaseTestCase extends WebTestCase
         );
     }
 
-    protected function createAuthentifiedClient(array $options = array(), array $server = array())
-    {
-        $client =  parent::createClient($options, $server);
-        $this->logIn($client);
 
-        return $client;
+    public function getUserManager()
+    {
+        return $this->container->get('fos_user.user_manager');
+
     }
+
+    public function getSecurityManager()
+    {
+        return $this->container->get('fos_user.security.login_manager');
+
+    }
+
 }
