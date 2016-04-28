78d52b075a15edbef9e511e4ba97e3d3744af2ed
Benjamin HUBERT <benjamin@alpixel.fr>
2016-04-28 14:33:45 +0200

    Re-enabled the sitemap listener

--- Listener/SitemapListener.php
+++ Listener/SitemapListener.php
@@ -2,64 +2,69 @@
 
 namespace Alpixel\Bundle\CMSBundle\Listener;
 
-use Alpixel\Bundle\SEOBundle\Event\SitemapPopulateEvent;
-use Alpixel\Bundle\SEOBundle\Service\SitemapListenerInterface;
-use Alpixel\Bundle\SEOBundle\Sitemap\Url\UrlConcrete;
-use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Presta\SitemapBundle\Event\SitemapPopulateEvent;
+use Presta\SitemapBundle\Service\SitemapListenerInterface;
+use Presta\SitemapBundle\Sitemap\Url\GoogleMultilangUrlDecorator;
+use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
 use Symfony\Component\Routing\RouterInterface;
 
 class SitemapListener implements SitemapListenerInterface
 {
-    protected $doctrine;
+    private $entityManager;
     private $router;
+    private $defaultLocale;
+    private $locales;
+    private $baseUrl;
 
-    public function __construct(Registry $doctrine, RouterInterface $router)
+    public function __construct(RouterInterface $router, EntityManager $entityManager, $defaultLocale, $locales, $baseUrl)
     {
-        $this->doctrine = $doctrine;
+        $this->entityManager = $entityManager;
         $this->router = $router;
+        $this->defaultLocale = $defaultLocale;
+        $this->locales = $locales;
+        $this->baseUrl = $baseUrl;
     }
 
     public function populateSitemap(SitemapPopulateEvent $event)
     {
         $section = $event->getSection();
+
         if (is_null($section) || $section == 'cms') {
-            $entities = [];
-            $entityManager = $this->doctrine->getManager();
-            $meta = $entityManager->getMetadataFactory()->getAllMetadata();
+            $nodeRepository = $this->entityManager->getRepository('AlpixelCMSBundle:Node');
+            $pages = $nodeRepository->findAllWithLocale($this->defaultLocale);
 
-            foreach ($meta as $m) {
-                $relations = $m->getAssociationMappings();
-                if (array_key_exists('node', $relations) && $relations['node']['targetEntity'] == 'Alpixel\Bundle\CMSBundle\Entity\Node') {
-                    $entities[] = $m;
-                }
-            }
-
-            $pages = [];
-            foreach ($entities as $entity) {
-                $objects = $entityManager
-                            ->getRepository($entity->getName())
-                            ->findAll();
-                foreach ($objects as $object) {
-                    if ($object->getPublished() === true) {
-                        $pages[] = $object;
-                    }
-                }
-            }
-
-            foreach ($pages as $cms) {
+            foreach ($pages as $page) {
                 $url = $this->router->generate('alpixel_cms', [
-                        'slug' => $cms->getSlug(),
-                    ],
-                    true
+                    'slug' => $page->getSlug(),
+                    '_locale' => $page->getLocale(),
+                ]);
+
+                $url = new UrlConcrete(
+                    $url,
+                    $page->getDateUpdated(),
+                    UrlConcrete::CHANGEFREQ_MONTHLY,
+                    .7
                 );
 
+                $urlLang = new GoogleMultilangUrlDecorator($url);
+                foreach ($this->locales as $locale) {
+                    if ($locale !== $this->defaultLocale) {
+                        $translatedPage = $nodeRepository->findTranslation($page, $locale);
+
+                        if($translatedPage !== null) {
+                            $url = $this->router->generate('alpixel_cms', [
+                                'slug' => $translatedPage->getSlug(),
+                                '_locale' => $translatedPage->getLocale(),
+                            ]);
+
+                            $urlLang->addLink($url, $locale);
+                        }
+                    }
+                }
+
                 $event->getGenerator()->addUrl(
-                    new UrlConcrete(
-                        $url,
-                        new \DateTime(),
-                        UrlConcrete::CHANGEFREQ_MONTHLY,
-                        .5
-                    ),
+                    $urlLang,
                     'cms'
                 );
             }
