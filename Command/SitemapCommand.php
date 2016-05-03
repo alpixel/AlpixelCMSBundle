14c3d8e8df85055b4842d526f5b36e7fbbf3597a
Benjamin HUBERT <benjamin@alpixel.fr>
2016-05-03 16:20:34 +0200

    debugging the sitemap command

--- Command/SitemapCommand.php
+++ Command/SitemapCommand.php
@@ -37,7 +37,7 @@ class SitemapCommand extends DumpSitemapsCommand implements ContainerAwareInterf
         $arguments['target']->setDefault($this->container->getParameter('kernel.root_dir') . '/../web/');
         $definition->setArguments($arguments);
 
-        if (!$input->hasArgument('target')) {
+        if (count($input->getArguments()) === 0) {
             $input = new ArgvInput(null, $definition);
             $input->setArgument('target', $this->container->getParameter('kernel.root_dir') . '/../web/');
         }
