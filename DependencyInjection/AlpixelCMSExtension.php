934e3de4aa72aec88c68f4c7c5d9ef27a7a738f9
Benjamin Hubert <benjamin@alpixel.fr>
2016-05-13 11:15:17 +0200

    creating the white label feature (#41)

--- DependencyInjection/AlpixelCMSExtension.php
+++ DependencyInjection/AlpixelCMSExtension.php
@@ -24,22 +24,22 @@ class AlpixelCMSExtension extends Extension implements PrependExtensionInterface
 
         foreach ($config['content_types'] as $name => $contentType) {
             if (empty($contentType['title'])) {
-                throw new InvalidConfigurationException('Content type '.$name.' shoud have a title');
+                throw new InvalidConfigurationException('Content type ' . $name . ' shoud have a title');
             }
 
             if (empty($contentType['description'])) {
-                @trigger_error('Content type '.$name.' shoud have a description', E_USER_WARNING);
+                @trigger_error('Content type ' . $name . ' shoud have a description', E_USER_WARNING);
             }
 
             if (!isset($contentType['class']) || empty($contentType['class']) || !class_exists($contentType['class'])) {
-                throw new InvalidConfigurationException('CMS '.$contentType['class'].' can\'t be found');
+                throw new InvalidConfigurationException('CMS ' . $contentType['class'] . ' can\'t be found');
             }
         }
         $container->setParameter('alpixel_cms.content_types', $config['content_types']);
 
         foreach ($config['blocks'] as $name => $contentType) {
             if (empty($contentType['title'])) {
-                throw new InvalidConfigurationException('Block '.$name.' shoud have a title');
+                throw new InvalidConfigurationException('Block ' . $name . ' shoud have a title');
             }
 
             if ((!isset($contentType['class']) || empty($contentType['class'])) && class_exists($this->_blockDefaultClass)) {
@@ -47,22 +47,33 @@ class AlpixelCMSExtension extends Extension implements PrependExtensionInterface
             }
 
             if (isset($contentType['class']) && !class_exists($contentType['class'])) {
-                throw new InvalidConfigurationException('Block '.$contentType['class'].' can\'t be found');
+                throw new InvalidConfigurationException('Block ' . $contentType['class'] . ' can\'t be found');
             }
         }
         $container->setParameter('alpixel_cms.blocks', $config['blocks']);
         $container->setParameter('alpixel_cms.exception_template', $config['exception_template']);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
 
     public function prepend(ContainerBuilder $container)
     {
         $parser = new Parser();
-        $config = $parser->parse(file_get_contents(__DIR__.'/../Resources/config/config.yml'));
+
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $bundleConfig = $this->processConfiguration(new Configuration(), $configs);
+
+        $config = $parser->parse(file_get_contents(__DIR__ . '/../Resources/config/config.yml'));
 
         foreach ($config as $key => $configuration) {
+            if ($key == "sonata_admin" && $bundleConfig['white_label'] === true) {
+                foreach ($configuration['dashboard']['blocks'] as $blockKey => $block) {
+                    if ($block['type'] === 'alpixel_cms.sonata.admin.block.alpixel') {
+                        unset($configuration['dashboard']['blocks'][$blockKey]);
+                    }
+                }
+            }
             $container->prependExtensionConfig($key, $configuration);
         }
     }
