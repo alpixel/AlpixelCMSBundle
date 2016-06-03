2bd6b61809a2240357af52ba4fe1fc3d5efdf9e6
Alexis Bussieres <alexis@alpixel.fr>
2016-06-03 15:16:40 +0200

    Create EventSlug

---
+++ AppEvents.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Alpixel\Bundle\CMSBundle;
+
+
+/**
+ * @author Alexis BUSSIERES <alexis@alpixel.fr>
+ */
+class AppEvents
+{
+    const SLUG_UPDATED = 'alpixelcmsbundle.event.slug.updated';
+}
