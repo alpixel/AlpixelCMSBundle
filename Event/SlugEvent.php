d43b41a355ac0b2699670f3e8d998003cfe1015e
Alexis Bussières <bussieres.alexis.dev@gmail.com>
2016-06-08 09:56:31 +0200

    Create EventSlug (#42)
    * Create EventSlug
    
    * Update and rename AppEvents.php to AlpixelCMSEvents.php

---
+++ Event/SlugEvent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Alpixel\Bundle\CMSBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+
+/**
+ * @author Alexis BUSSIERES <alexis@alpixel.fr>
+ *
+ * The SlugEvent class is used for an entity which have a slug
+ * shared between different entities.
+ */
+class SlugEvent extends Event
+{
+    private $entity;
+    private $property;
+
+    /**
+     * SlugEvent constructor.
+     * @param $entity object
+     * @param $property string
+     */
+    public function __construct($entity, $property)
+    {
+        if (!is_object($entity)) {
+            throw new \InvalidArgumentException('The "$entity" parameter must be an object.');
+        } elseif (!empty($property) && is_string($property)) {
+            throw new \InvalidArgumentException('The "$property" parameter must be a non empty string.');
+        }
+
+        $this->entity = $entity;
+        $this->property = $property;
+    }
+
+    /**
+     * @return object
+     */
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProperty()
+    {
+        return $this->property;
+    }
+}
